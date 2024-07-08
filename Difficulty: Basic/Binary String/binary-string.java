e08c4fbba3b26379a581d96f57c6464b1bbc184d
PratikGangle <111277831+PratikGangle@users.noreply.github.com>
2024-07-08 20:08:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Binary String/binary-string.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG{
+    public static Scanner sc = new Scanner(System.in);
+	public static void main (String[] args){
+        int t;
+    	t=sc.nextInt();
+    	sc.nextLine();
+    	while((t--)!=0){
+    	    int n=sc.nextInt();
+    	    sc.nextLine();
+    	    String s = new String();
+    	    s=sc.nextLine();
+    	    Solution obj = new Solution();
+    	    System.out.println(obj.binarySubstring(n, s));
+    	}
+	}
+}
+// } Driver Code Ends
+
+
+
+
+class Solution
+{
+    //Function to count the number of substrings that start and end with 1.
+    public static int binarySubstring(int a, String str)
+    {
+        // Your code here
+        int countOne = 0;
+        
+        for(int i=0; i<a; i++){
+            char c = str.charAt(i);
+            if(c == '1'){
+                countOne++;
+            }
+        }
+        
+        return (countOne * (countOne-1))/2 ;
+    }
+}
